3afb5129bc2062edb6007bb5e2c30d9391b1f69d
mhdfahrurozi <mhdfahrurozi5@gmail.com>
2025-08-25 22:29:01 +0700

    update

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -11,15 +11,6 @@ class HomeController extends Controller
 {
     public function index()
     {
-        dd([
-        'APP_URL from env()' => env('APP_URL'),
-        'APP_URL from config()' => config('app.url'),
-        'Current URL scheme' => URL::current(),
-        'Asset URL example' => asset('test'),
-        'Full application URL' => URL::to('/'),
-        'Is HTTPS?' => request()->isSecure(),
-        'Environment' => App::environment()
-        ]);
         
         return view('home' , [
             'faqs'=>Faq::all()
