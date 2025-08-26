7d14a710a9e12d6d22375b4af630a4d1faf8a0ef
mhdfahrurozi <mhdfahrurozi5@gmail.com>
2025-08-26 23:08:44 +0700

    test

--- app/Http/Controllers/DashboardPostController.php
+++ app/Http/Controllers/DashboardPostController.php
@@ -52,7 +52,7 @@ class DashboardPostController extends Controller
             'image' => 'required|image|file|max:3072'
         ]);
 
-        // agar storage di simpan di public
+        // agar storage di simpan di public dan dapat diakses
         $path = $request->file('image')->store('post-cover-images', 'public');
         $validatedData['image'] = $path;
         $validatedData['user_id'] = auth()->user()->id;
